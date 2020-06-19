504d9e9e3cb3c115b4f8872e06669bcfcb21896d
WataruSuzuki <wataru0406@hotmail.co.jp>
2020-06-19 22:20:00 +0900

    Added example class

---
+++ CodeCovExample/Classes/CDCVExample.h
@@ -0,0 +1,19 @@
+//
+//  CDCVExample.h
+//  CodeCovExample
+//
+//  Created by 鈴木航 on 2020/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CDCVExample : NSObject
+
++ (NSString *)helloStaticMethod;
+- (NSString *)helloDynamicMethod;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CodeCovExample/Classes/CDCVExample.m
@@ -0,0 +1,20 @@
+//
+//  CDCVExample.m
+//  CodeCovExample
+//
+//  Created by 鈴木航 on 2020/08/24.
+//
+
+#import "CDCVExample.h"
+
+@implementation CDCVExample
+
++ (NSString *)helloStaticMethod {
+    return @"Hello world";
+}
+
+- (NSString *)helloDynamicMethod {
+    return @"Hello world";
+}
+
+@end
